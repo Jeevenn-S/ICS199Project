105a8b5612bb6ae7efe06aa3e54eb26d1fe8387e
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-05-10 12:18:33 -0700

    Basic webpages created + css + folders + favicon

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fresh Threads - Home</title>
+    <link rel="icon" href="../images/favicon.png" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
+
+</head>
+<body>
+<div class="navbar">
+    <a class="active" href="index.php">Home</a>
+    <a href="shop.php">Shop</a>
+</div>
+</body>
+</html>
---
+++ php/shop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fresh Threads - Shop</title>
+    <link rel="icon" href="../images/favicon.png" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="../css/shop.css">
+
+</head>
+<body>
+<div class="navbar">
+    <a href="index.php">Home</a>
+    <a class="active" href="shop.php">Shop</a>
+</div>
+</body>
+</html>
